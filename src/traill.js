24a575ba454d05139de2441c9f9231ef12da766d
Dineshkumar-2001 <dineshkumar@valorpaytech.com>
2022-06-10 13:00:55 +0530

    done

--- src/traill.js
+++ src/traill.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class traill extends Component {
   render() {
     return (
+        <div>
       <div>traill complete</div>
+      <div>traill sucess</div>
+      </div>
     )
   }
 }
